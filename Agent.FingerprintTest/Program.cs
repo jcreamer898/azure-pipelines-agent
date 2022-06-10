781208ac58c896ce662472ae7f2a253bbdb5ae71
Jonathan Creamer <jcreamer@microsoft.com>
2022-06-10 15:16:23 -0500

    create a way to run the fingerprint code

---
+++ Agent.FingerprintTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using Agent.Plugins.PipelineCache;
+using Agent.Sdk;
+using System;
+
+namespace Agent.FingerprintTest
+{
+    class Test
+    {
+        public Test() { }
+
+        public void Run(string dir)
+        {
+            using (var hostContext = new TestHostContext(this))
+            {
+                var context = new AgentTaskPluginExecutionContext(hostContext.GetTrace());
+                context.SetVariable("system.defaultworkingdirectory", @"D:\1JS\midgard");
+                
+                FingerprintCreator.EvaluateKeyToFingerprint(context, dir , new[] { "**/Podfile.lock" });
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test();
+            test.Run(@"D:\1JS\midgard");
+        }
+    }
+}
